a5063e0282c9925f851ca7fc8fb297f564f21a7f
b1ank002 <vlad.hotenko@gmail.com>
2025-07-02 00:36:46 +0200

    feat: Added openZeppelin lib

--- src/ERC20tokenForTest.sol
+++ src/ERC20tokenForTest.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {ERC20} from "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract BlankERC20 is ERC20, ERC20Permit {
     constructor(address recipient) ERC20("Blank", "BLK") ERC20Permit("Blank") {
